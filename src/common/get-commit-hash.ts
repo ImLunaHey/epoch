6b7c7e2eaba8d16e11cb807cd34f5944ee58b2bb
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-05-27 04:29:51 +0930

    chore: log when getting hash

--- src/common/get-commit-hash.ts
+++ src/common/get-commit-hash.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { env } from '@app/common/env';
 
 const getHashFromDisk = () => {
+    console.log('getting hash from disk');
     try {
         const fileContents = fs.readFileSync('.git/HEAD').toString();
         const rev = fileContents.trim().split(/.*[: ]/).slice(-1)[0];
@@ -13,10 +14,14 @@ const getHashFromDisk = () => {
     return null;
 };
 
-const getHashFromEnv = () => env.GIT_COMMIT_SHA;
+const getHashFromEnv = () => {
+    console.log('getting hash from env');
+    return env.GIT_COMMIT_SHA;
+};
 
 let commitHash: string;
 export const getCommitHash = () => {
+    console.log('getting hash');
     if (commitHash) return commitHash;
     commitHash = (getHashFromEnv() ?? getHashFromDisk() ?? 'unknown').substring(0, 12);
     return commitHash;
